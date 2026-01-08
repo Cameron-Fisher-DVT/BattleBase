68137111e2d71dbb7cb0e3829a1b5a87770f7056
Cameron-Fisher-DVT <cfisher@jhb.dvt.co.za>
2026-01-08 10:17:19 +0200

    [Feature][04] - Navigation
    Added Jetpack Navigation 3
    Added MenuScreen to test the navigation

--- app/src/main/java/za/co/dvt/battlebase/common/presentation/MainActivity.kt
+++ app/src/main/java/za/co/dvt/battlebase/common/presentation/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavBackStack
+import za.co.dvt.battlebase.common.presentation.navigation.Destination
+import za.co.dvt.battlebase.common.presentation.navigation.Navigation
 import za.co.dvt.battlebase.common.presentation.ui.theme.BattleBaseTheme
 import za.co.dvt.battlebase.features.home.presentation.HomeScreen
 import za.co.dvt.battlebase.features.home.presentation.HomeScreenViewModel
@@ -18,12 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BattleBaseTheme {
-                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
-                    val homeScreenViewModel = HomeScreenViewModel()
-                    HomeScreen(modifier = Modifier.padding(innerPadding), snackbarHostState = homeScreenViewModel.snackbarHostState) {
-                        homeScreenViewModel.displaySnackbar("Menu")
-                    }
-                }
+                Navigation(NavBackStack(Destination.HomeScreen))
             }
         }
     }
