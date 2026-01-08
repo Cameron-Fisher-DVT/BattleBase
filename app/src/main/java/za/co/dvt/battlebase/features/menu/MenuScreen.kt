7c0fba849861c38e7ab44d8c6dbf1db0bcb6ecae
Cameron-Fisher-DVT <cfisher@jhb.dvt.co.za>
2026-01-08 11:15:05 +0200

    [Feature][09] - Color Scheme
    Added Color scheme for light and dark mode.

--- app/src/main/java/za/co/dvt/battlebase/features/menu/MenuScreen.kt
+++ app/src/main/java/za/co/dvt/battlebase/features/menu/MenuScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -26,7 +28,13 @@ fun MenuScreen(
                 title = { Text(stringResource(R.string.battle_base_menu)) },
                 actions = {
 
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onTertiaryContainer
+                )
             )
         }
     ) { padding ->
